f055041d5001704eb3f170e603b6dd94ce23c545
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-22 16:22:21 +0100

    milestone 2 finished

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
-        Project List
+      <h1>My Projects</h1>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          {{ project.name }}
+        </li>
+      </ul>
     </div>
-</template>
-
-<script>
-    export default {
-        name: 'AppProjects',
+  </template>
+  
+  <script>
+  import { store } from "../store";
+  import axios from "axios";
+  
+  export default {
+    name: "AppProjects",
+    data() {
+      return {
+        store,
+        projects: [],
+      };
+    },
+    methods: {
+      getAllProjects() {
+        axios.get(store.apiUrl + "/projects").then((response) => {
+          console.log(response.data.data);
+          this.projects = response.data.data;
+          console.log(this.projects);
+        });
+      },
+    },
+    created(){
+      this.getAllProjects()
     }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
