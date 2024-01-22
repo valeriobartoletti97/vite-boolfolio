9509ef38514fedcaaaff95890f55d691aafe3dbc
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-22 16:57:55 +0100

    milestone 3 finished - added projectcard

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="d-flex justify-content-center mt-5">
+        <div class="card" style="width: 18rem">
+          <img :src="image" class="card-img-top" alt="project-img"/>
+          <div class="card-body text-center">
+            <h5 class="card-title text-uppercase fs-5">{{title}}</h5>
+            <p class="card-text">
+                <span class="badge text-bg-success">{{language}}</span>
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  props: [
+    "image", 
+    "title",
+    "language"
+  ]
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,12 +1,38 @@
 <template>
-    <div>
-        Single Project 
-    </div>
+    <h1>
+        Single Project
+    </h1>
+    <ProjectCard :image="this.project.image" :title="this.project.name" :language="this.project.language" />
 </template>
 
 <script>
+    import { store } from "../store";
+    import axios from "axios";
+    import ProjectCard from '../components/ProjectCard.vue'
     export default {
         name: 'SingleProject',
+        components:{
+            ProjectCard
+        },
+        data(){
+            return{
+                store,
+                project: null,
+            }
+        },
+        methods:{
+            getProjectData(){
+                console.log(this.$route)
+                axios.get(store.apiUrl + "/projects/" + this.$route.params.id).then((response) =>{
+                    console.log(response.data.data[0])
+                    this.project = response.data.data[0]
+                    console.log(this.project)
+                })
+            }
+        },
+        created(){
+            this.getProjectData()
+        }
     }
 </script>
 
